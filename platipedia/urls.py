d04e9b2cf158ad82a05ca138410d20195a13667c
laysquad <finaljuan@hotmail.com>
2018-11-07 16:41:44 -0600

    Septimo commit

--- platipedia/urls.py
+++ platipedia/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.Principal, name='Principal'),
+    path('platillodetalle/<int:pk>/', views.PlatilloDetalle, name='PlatilloDetalle'),
+    path('ingredientedetalle/<int:pk>/', views.IngredienteDetalle, name='IngredienteDetalle'),
 ]
--- platipedia/views.py
+++ platipedia/views.py
@@ -5,4 +5,13 @@ from platipedia.models import Platillo, Ingrediente
 def Principal(request):
     ListaPlatillos = Platillo.objects.all()
     ListaIngredientes = Ingrediente.objects.all()
-    return render(request, 'platipedia/base.html', {'ListaPlatillos':ListaPlatillos,'ListaIngredientes':ListaIngredientes})
+    return render(request, 'platipedia/principal.html', {'ListaPlatillos':ListaPlatillos,'ListaIngredientes':ListaIngredientes})
+
+def PlatilloDetalle(request,pk):
+    vPlatillo = Platillo.objects.filter(id=pk)[0]
+    vIngredientes = vPlatillo.ingredientes.all()
+    return render(request, 'platipedia/PlatilloDetalle.html', {'Platillo':vPlatillo, 'Ingredientes':vIngredientes})
+
+def IngredienteDetalle(request,pk):
+    vIngrediente = Ingrediente.objects.filter(id=pk)[0]
+    return render(request, 'platipedia/IngredienteDetalle.html', {'Ingrediente':vIngrediente})
